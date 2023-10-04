fe80ecb37e343b2d44216f7afdf8e0ba1caf4c02
zLuisTv <luisone06@gmail.com>
2023-10-04 06:39:26 -0500

    error vercel

--- components/Theme/ThemeUtil.tsx
+++ components/Theme/ThemeUtil.tsx
@@ -1,8 +1,18 @@
 export const getDarkModeFromLocalStorage = () => {
+    if (typeof window === 'undefined') {
+      // Verificar si estamos en el entorno del servidor
+      return false; // En el servidor, no hay acceso a localStorage
+    }
+  
     const storedDarkMode = localStorage.getItem('darkMode');
     return storedDarkMode ? JSON.parse(storedDarkMode) : false;
-};
-
-export const setDarkModeToLocalStorage = (darkMode: boolean) => {
+  };
+  
+  export const setDarkModeToLocalStorage = (darkMode: boolean) => {
+    if (typeof window === 'undefined') {
+      // Verificar si estamos en el entorno del servidor
+      return; // En el servidor, no hay acceso a localStorage
+    }
+  
     localStorage.setItem('darkMode', JSON.stringify(darkMode));
-};
+  };
